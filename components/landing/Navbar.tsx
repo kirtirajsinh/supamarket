5ff46d6d11037bdcf683f7925f73c29d22b4f270
kirtirajsinh <kirtiraj3738@gmail.com>
2024-10-21 14:12:59 +0530

    Refactor Navbar component to use Next.js Link component for better navigation

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import LoginButton from "../auth/LoginButton";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <div className="z-50  fixed top-8 left-1/2 transform -translate-x-1/2 inline-flex mx-auto justify-between w-8/12 sm:w-2/5 rounded-full backdrop-blur-xl border border-1 border-blue-500/10 backdrop-filter bg-blue-900/10 pl-8 pr-8 py-3 items-center ">
-      <h1 className="text-2xl font-bold">supamarket</h1>
+      <Link href="/" className="text-2xl font-bold">
+        supamarket
+      </Link>
       <LoginButton />
     </div>
   );
